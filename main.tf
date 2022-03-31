ec91a258c799bd0745137dcfb7a5a89bf73d6f40
StsyNo2 <happy39mail@gmail.com>
2022-03-31 16:55:03 +0900

    modified to t2.micro

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "devinstance" {
 	source  = "app.terraform.io/s_nonomiya_org/devinstance/aws"
 	version = "0.0.2"
 
-	hello_tf_instance_type = "t2.large"
+	hello_tf_instance_type = "t2.micro"
         prefix = "satoshi.nonomiya"
 }
 
