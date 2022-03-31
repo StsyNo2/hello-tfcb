ca66cf40ddbc53ad9ca1ec1ecccfe4e3a02deea9
StsyNo2 <happy39mail@gmail.com>
2022-03-31 16:30:07 +0900

    modularization

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
-output "ip_address" {
-  value = aws_eip.hashicat.public_ip
+output "eip" {
+  value = module.devinstance.eip
 }
+
+output "instance-info" {
+  value = module.devinstance.instance-info
+}
+}
+
